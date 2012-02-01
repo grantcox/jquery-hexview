d8d38709c7c40fe13092257fc324a5f7d0602144
Grant Cox <grant.cox@gmail.com>
2012-02-01 17:15:28 -0500

    Chunk values displayed on hover, with endian option, in hex, binary or decimal

--- jquery.hexview.js
+++ jquery.hexview.js
@@ -1,57 +1,84 @@
 (function( $ ) {
 	$.fn.hexView = function(options) {
 		var settings = $.extend( {
-			'split': 4,
-			'columns': 8,
+			'bytesPerColumn': 4,
+			'columns': 4,
 			'address': 1280,
 			'showAddress': true,
+			'littleEndian': true,
 			'hover': {'decimal': true, 'binary':true, 'address':true}
 		}, options);
 		var i;
 		
-		chunks = this.text().match(RegExp('.{1,' + settings.split + '}','g'));
-		cols = [];
+		var bytes = this.text().match(RegExp('[0-9A-Fa-f]{2}\s?','g'));
+		var cols = [];
 		
 		this.html('')
 		
+		var start_address = 0 + settings.address;
+		var rowsize = settings.bytesPerColumn * settings.columns;
+		var rows = bytes.length / rowsize;
+		
 		// add the column of addresses
 		if (settings.showAddress) {
-			start = 0 + settings.address;
-			rowsize = settings.split * settings.columns;
-			rows = chunks.length / settings.columns;
-			
-			console.log(rows);
-			console.log(rowsize);
-			console.log(start);
-			
-			col = $('<div/>', {
+			var col = $('<div/>', {
 				'class': 'hex-addresses'
 			});
 			this.append(col);
 			
 			for (i=0; i<rows; i++){
-				col.append('<span class="hex-chunk">@'+(start + rowsize*i)+'</span>');
+				col.append('<span class="hex-address">@'+(start_address + rowsize*i)+'</span>');
 			}
 		}
 		
 		// add the columns for the chunks
 		for (i=0; i<settings.columns; i++){
-			d = $('<div/>', {
+			var d = $('<div/>', {
 				'class': 'hex-col ' + (i%2==0 ? 'hex-col-even' : 'hex-col-odd')
 			});
 			cols.push(d);
 			this.append(d);
 		}
 		
-		// add the chunks
-		for (i=0; i<chunks.length; i++){
-			cols[i%settings.columns].append('<span class="hex-chunk">'+chunks[i]+'</span>');
+		// add the bytes to chunks, and the chunks to columns
+		var chunkSpan;
+		for (i=0; i<bytes.length; i++){
+			if (i % settings.bytesPerColumn == 0){
+				chunkSpan = $('<span/>', {
+					'class': 'hex-chunk'
+				});
+				chunkSpan.data('address', (start_address + i));
+				
+				console.log(i, settings.bytesPerColumn, (i % settings.bytesPerColumn), rowsize, (i % rowsize), settings.columns);
+				
+				cols[(i % rowsize) / settings.bytesPerColumn].append(chunkSpan);
+			}
+			var byteSpan = $('<span/>', {
+				'class': 'hex-byte',
+				'text': bytes[i]
+			});
+			chunkSpan.append(byteSpan);
 		}
 		
 		// and if we need any hover functionality, add that
 		if (settings.hover) {
-			this.append('<div class="hover-footer">hi there</div>');
-			//this.hover(function(){})
+			this.append('<div class="hover-footer">sd</div>');
+			this.find('span.hex-chunk').hover(function(e){
+				$(this).addClass('over');
+				
+				var stringRep = $(this).text()
+				if (settings.littleEndian){
+					stringRep = ''
+					$.each($(this).children().get().reverse(),
+						function(i, v){stringRep += $(v).text();}
+					);
+				}
+				var val = parseInt(stringRep, 16);
+				var hover_string = "@" + $(this).data('address') + ' 0x' + stringRep + ' bin: ' + val.toString(2) + ' dec: ' + val.toString(10);
+				$(".hover-footer").text(hover_string);
+			},function(){
+				$(this).removeClass('over');
+			})
 		}
 	};
 })( jQuery );
